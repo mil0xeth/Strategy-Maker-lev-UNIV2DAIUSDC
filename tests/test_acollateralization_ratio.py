fd06564291c7ddc5b1f5e91f7bee54a131aaefe9
Richard Liang <richardliang2015@u.northwestern.edu>
2022-08-07 15:02:23 -0700

    Fix acollateralization test

--- tests/test_acollateralization_ratio.py
+++ tests/test_acollateralization_ratio.py
@@ -65,7 +65,7 @@ def test_vault_ratio_calculation_on_BIGTIME_total_withdraw(
 
 
     # Withdraw 100% of the assets, with 0.1% maxLoss
-    withdraw_tx = vault.withdraw(amountBIGTIME*0.125, user, 1000, {"from": user})
+    withdraw_tx = vault.withdraw(amountBIGTIME*0.2, user, 1000, {"from": user})
     test_strategy.harvest({"from": gov})
 
     # Strategy should restore collateralization ratio to target value on withdraw
